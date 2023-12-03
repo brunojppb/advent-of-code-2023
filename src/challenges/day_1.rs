3a997d8e19f6df707db6b77a8b94c31e8db6540a
Bruno Paulino <bruno@bpaulino.com>
2023-12-03 21:51:32 +0100

    Day 1 - part 2

--- src/challenges/day_1.rs
+++ src/challenges/day_1.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 pub struct Day1 {}
 
@@ -8,12 +8,12 @@ impl Day1 {
     }
 
     pub fn run(self) {
-        self.part_1()
+        self.part_1().part_2();
     }
 
     // See: https://adventofcode.com/2023/day/1
-    fn part_1(self) {
-        let contents = fs::read_to_string("inputs/day_1.txt").unwrap();
+    fn part_1(self) -> Self {
+        let contents = fs::read_to_string("inputs/day_1_part_1.txt").unwrap();
         let mut numbers: Vec<usize> = Vec::with_capacity(contents.lines().count());
 
         for line in contents.lines() {
@@ -40,7 +40,56 @@ impl Day1 {
             }
         }
 
-        let result: usize = numbers.iter().sum();
-        println!("Result: {}", result)
+        println!("Part 1 - Result: {}", numbers.iter().sum::<usize>());
+        self
+    }
+
+    fn part_2(self) -> Self {
+        let digits = HashMap::from([
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four", 4),
+            ("five", 5),
+            ("six", 6),
+            ("seven", 7),
+            ("eight", 8),
+            ("nine", 9),
+        ]);
+
+        let contents = fs::read_to_string("inputs/day_1_part_2.txt").unwrap();
+        let mut numbers: Vec<usize> = Vec::new();
+
+        for line in contents.lines() {
+            let mut acc = String::new();
+            let size = line.chars().count();
+            for i in 0..size {
+                let char = line.chars().nth(i).unwrap();
+                if char.is_ascii_digit() {
+                    acc = format!("{}{}", acc, char);
+                    continue;
+                }
+
+                for (key, num) in &digits {
+                    let range = i..(i + (key.len()));
+                    if size - i >= key.len() {
+                        let slice = &line[range];
+                        if &slice == key {
+                            acc = format!("{}{}", acc, num);
+                            continue;
+                        }
+                    }
+                }
+            }
+            let (first, last) = (acc.chars().nth(0), acc.chars().nth(acc.len() - 1));
+            numbers.push(
+                format!("{}{}", first.unwrap(), last.unwrap())
+                    .parse()
+                    .unwrap(),
+            );
+        }
+
+        println!("Part 2 - Result: {}", numbers.into_iter().sum::<usize>());
+        self
     }
 }
